4412a21a6d83eabbf568223ae18f6f8a6e10ad5e
Richard Loera <richexp9@gmx.com>
2017-12-04 06:02:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const app = "I don't do much."
 
 var kittens = ['Milo', 'Otis', 'Garfield']
-// 
-// function destructivelyAppendKitten(name){
-//   kittens.push(name)
-// }
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
+}
 
 function destructivelyPrependKitten(name) {
   kittens.unshift(name)
